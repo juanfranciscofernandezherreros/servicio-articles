3104e32b8a812779fe76bc782381ae0bf99d51dc
jffernandez.herreros <juanfrancisco.fernandezherreros@colaborador.viajeseci.es>
2021-03-05 12:35:11 +0100

    optimize imports

--- src/main/java/com/fernandez/api/articles/service/impl/UserServiceImpl.java
+++ src/main/java/com/fernandez/api/articles/service/impl/UserServiceImpl.java
@@ -1,9 +1,7 @@
 package com.fernandez.api.articles.service.impl;
 
 import com.fernandez.api.articles.common.Messages;
-import com.fernandez.api.articles.constants.PropertiesConstant;
 import com.fernandez.api.articles.dto.UserDTO;
-import com.fernandez.api.articles.exceptions.ArticlesLogicException;
 import com.fernandez.api.articles.model.User;
 import com.fernandez.api.articles.repository.UserRepository;
 import com.fernandez.api.articles.service.UserService;
@@ -11,11 +9,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 import org.modelmapper.ModelMapper;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.Objects;
 
 
 @Slf4j
